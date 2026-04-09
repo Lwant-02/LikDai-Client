befa9848a84185b28d4d402907fdfff32de9c969
Lwant <155357373+Lwant-02@users.noreply.github.com.>
2026-04-09 16:39:27 +0700

    fix:paw auto update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { registerSW } from "virtual:pwa-register";
 
 import "./index.css";
 import "./lib/i18n.ts";
@@ -9,6 +10,17 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient.ts";
 import { AuthProvider } from "./components/layout/AuthProvider.tsx";
 import { ThemeProvider } from "./components/layout/ThemeProvider.tsx";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload page to update?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("PWA ready for offline use");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
